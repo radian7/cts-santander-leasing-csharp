e3f1a452924e6d9fe5c21f86b3070b7164d02677
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-07-22 14:27:10 +0200

    Database First

---
+++ src/SantanderLeasing.DotnetCore/SantanderLeasing.DotnetCore.DbServices/DbCustomerService.cs
@@ -0,0 +1,61 @@
+﻿using SantanderLeasing.DotnetCore.IServices;
+using SantanderLeasing.DotnetCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SantanderLeasing.DotnetCore.DbServices
+{
+    public class DbCustomerService : ICustomerService
+    {
+        public void Add(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddAsync(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Customer>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/SantanderLeasing.DotnetCore/SantanderLeasing.DotnetCore.DbServices/Model/VGetAllCategories.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SantanderLeasing.DotnetCore.DbServices.Model
+{
+    public partial class VGetAllCategories
+    {
+        public string ParentProductCategoryName { get; set; }
+        public string ProductCategoryName { get; set; }
+        public int? ProductCategoryId { get; set; }
+    }
+}
