ecf02dd6961d890afdafc5b4ae941fd4e0d882d9
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-07-23 10:07:00 +0200

    Faker Bogus

---
+++ src/SantanderLeasing.DotnetCore/SantanderLeasing.DotnetCore.FakeServices/FakeProductService.cs
@@ -0,0 +1,50 @@
+﻿using Bogus;
+using SantanderLeasing.DotnetCore.IServices;
+using SantanderLeasing.DotnetCore.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SantanderLeasing.DotnetCore.FakeServices
+{
+
+    public class FakeProductService : IProductService
+    {
+        private readonly ICollection<Product> products;
+
+        public FakeProductService(Faker<Product> faker)
+        {
+            products = faker.Generate(100);
+        }
+
+        public void Add(Product entity)
+        {
+            products.Add(entity);
+        }
+
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+
+        public Product Get(int id)
+        {
+            return products.SingleOrDefault(p => p.Id == id);
+        }
+
+        public IEnumerable<Product> GetByColor(string color)
+        {
+            return products.Where(p => p.Color == color);
+        }
+
+        public void Remove(int id)
+        {
+            products.Remove(Get(id));
+        }
+
+        public void Update(Product entity)
+        {
+            Remove(entity.Id);
+            Add(entity);
+        }
+    }
+}
---
+++ src/SantanderLeasing.DotnetCore/SantanderLeasing.DotnetCore.FakeServices/Fakers/ProductFaker.cs
@@ -0,0 +1,19 @@
+﻿using Bogus;
+using SantanderLeasing.DotnetCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SantanderLeasing.DotnetCore.FakeServices.Fakers
+{
+    public class ProductFaker : Faker<Product>
+    {
+        public ProductFaker()
+        {
+            RuleFor(p => p.Id, f => f.IndexFaker);
+            RuleFor(p => p.Name, f => f.Commerce.ProductName());
+            RuleFor(p => p.Color, f => f.Commerce.Color());
+            RuleFor(p => p.UnitPrice, f => decimal.Parse(f.Commerce.Price()));
+        }
+    }
+}
