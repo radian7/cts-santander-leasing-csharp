7ca02627c7692b887a3b5a66bdf4f7a5bb76ce12
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-07-23 09:54:03 +0200

    Clean

--- src/SantanderLeasing.DotnetCore/SantanderLeasing.DotnetCore.IServices/IEntityServiceAsync.cs
+++ src/SantanderLeasing.DotnetCore/SantanderLeasing.DotnetCore.IServices/IEntityServiceAsync.cs
@@ -11,25 +11,4 @@ namespace SantanderLeasing.DotnetCore.IServices
         Task UpdateAsync(TEntity entity);
         Task RemoveAsync(int id);
     }
-
-
-    /* separacja interfejsów
-    public interface IEntityService<TEntity> : IReadEntityService<TEntity>, IWriteEntityService<TEntity>
-    {
-    }
-
-    public interface IReadEntityService<TEntity>
-    {
-        IEnumerable<TEntity> Get();
-        TEntity Get(int id);
-    }
-
-    public interface IWriteEntityService<TEntity>
-    {
-        void Add(TEntity entity);
-        void Update(TEntity entity);
-        void Remove(int id);
-    }
-
-    */
 }
